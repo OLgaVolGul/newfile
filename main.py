832a2815dd7f0622a4f873ab5ef17fc3dfd914e6
OLgaVolGul <olgavolgul@gmail.com>
2025-12-22 21:50:42 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,49 @@
+# print("Hello World!")
+# print (9+12)
+# num = 2
+# num2= 1
+# name = "Tim"
+# print(num + num2)
+# print ("Hello ", name)
+# print ("1"+"2")
+# print (int("1")+2)
+# age=25
+# name="alex"
+# print(f"Привет, меня зовут {name}. Мне {age} лет")
+# print ("test" == )
+# test = 1+2+3
+# print(test)
+# test1=6-4
+# print(test1)
+# print(test*test1)
+# print(8/3)
+# int1=8
+# int2=3
+# float1=int1/int2
+# print (int(float1))
+# num11=5
+# num21=2
+# res1=num11+num21
+# print(res1)
+# num22=7
+# num23=3
+# res2=num22*num23
+# print(res2)
+# num31=15
+# num32=6
+# res3=num31/num32
+# print(res3)
+# print (int(res3))
+# num33=4
+# num33 += 1
+# num33 -=1
+# print(num33)
+# age1=int(input())
+#tst=5
+# if tst > 10:
+# 	print("+++")
+# else:
+# 	print("---")
+def func():
+	print("Olga Gul")
+func()
